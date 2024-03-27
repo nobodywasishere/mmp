100a912e9de0e889539405c7f6763357e6d8117b
Margret Riegert <margret@eowyn.net>
2024-03-27 13:26:34 -0400

    Working MMP firmware

--- firmware/targets/mmp/main.go
+++ firmware/targets/mmp/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"context"
 	_ "embed"
+	"image/color"
 	"machine"
 
 	keyboard "github.com/sago35/tinygo-keyboard"
 	"github.com/sago35/tinygo-keyboard/keycodes/jp"
+	"tinygo.org/x/drivers/ws2812"
 )
 
 func main() {
+	neo := machine.WS2812
+	neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	wsLeds := [1]color.RGBA{}
+	wsLeds[0] = color.RGBA{0x05, 0x00, 0x00, 0x05}
+
+	ws := ws2812.New(neo)
+	ws.WriteColors(wsLeds[:])
+
 	d := keyboard.New()
 
 	rowPins := []machine.Pin{
@@ -27,9 +38,10 @@ func main() {
 
 	d.AddMatrixKeyboard(colPins, rowPins, [][]keyboard.Keycode{
 		{
-			jp.KeyA, jp.KeyB, jp.KeyC, jp.KeyD,
-			jp.KeyA, jp.KeyB, jp.KeyC, jp.KeyD,
-			jp.KeyA, jp.KeyB, jp.KeyC, jp.KeyD,
+			jp.KeyA, jp.KeyB, jp.KeyC,
+			jp.KeyD, jp.KeyE, jp.KeyF,
+			jp.KeyG, jp.KeyH, jp.KeyI,
+			jp.KeyJ, jp.KeyK, jp.KeyL,
 		},
 	})
 
